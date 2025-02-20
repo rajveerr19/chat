529a19c57a4f039a48e52cec46ca34d54dcddd52
rajveerr19 <websiteonly7@gmail.com>
2025-02-20 22:54:01 +0530

    ok

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 // User Schema
 const userSchema = new mongoose.Schema({
